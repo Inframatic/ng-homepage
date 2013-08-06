2ff4bcbf8e6249c942406f9b5cdff8696b521891
Inframatic <isteffy@hotmail.com>
2013-08-06 14:24:25 -0400

    added jquery command to toggle slide the sidebar

--- scripts/homepagecontroller.js
+++ scripts/homepagecontroller.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
    $(".slider_head").click(function(){
      $(this).siblings(".panel").slideToggle("fast");
    });
+
+  $('#toggleaside').click(function(){
+    $('aside').toggle("slide");
+  });
 });
